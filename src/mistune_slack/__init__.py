133700007ec42de51ebb8fd99fa4315c879ca2d4
Ryan Munro <500774+munro@users.noreply.github.com>
2025-05-06 21:49:34 -0400

    Update pypi package metadata

--- src/mistune_slack/__init__.py
+++ src/mistune_slack/__init__.py
@@ -2,7 +2,7 @@ from mistune_slack.plugin import slack_plugin
 from mistune_slack.renderer import SlackRenderer
 from mistune_slack.util import render_slack_blocks_from_markdown, slack_markdown_renderer
 
-VERSION = __version__ = "0.1.0"
+VERSION = __version__ = "0.1.1"
 
 __all__ = [
     "render_slack_blocks_from_markdown",
